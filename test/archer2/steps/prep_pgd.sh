8f726b7fce105ccd8a8bddea744d310b895269b6
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-02-11 13:26:24 +0000

    move archer -> archer2 and make step prep_pgd work

---
+++ test/archer2/steps/prep_pgd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --export=none
+#SBATCH --job-name=prep_pgd
+#SBATCH --time=0:05:00
+#SBATCH --partition=standard
+#SBATCH --qos=short
+#SBATCH --reservation=shortqos
+#SBATCH --account=d170
+#SBATCH --nodes=1
+
+. ../env.sh
+
+cwltool \
+    --beta-dependency-resolvers-configuration $VESTEC_CWL_PLATFORM_CONF \
+    --enable-ext \
+    --preserve-environment LD_LIBRARY_PATH \
+    --mpi-config-file $VESTEC_CWL_MPI_CONF \
+    $VESTEC_CWL_ROOT/wildfire/prep_pgd.cwl \
+    prep_pgd.yml
