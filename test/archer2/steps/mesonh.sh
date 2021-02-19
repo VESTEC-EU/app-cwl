bb26aeecc1686c2f6c3caadce42cede8513aca73
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-02-19 14:09:05 +0000

    interim adaptation of mesonh, pending fixes

--- test/archer2/steps/mesonh.sh
+++ test/archer2/steps/mesonh.sh
@@ -1,16 +1,20 @@
-#!/bin/bash --login
-#PBS -N runmesonh
-#PBS -l select=1
-#PBS -l walltime=0:05:00
-#PBS -q short
-#PBS -A d170
-
-cd $PBS_O_WORKDIR
+#!/bin/bash
+#SBATCH --export=none
+#SBATCH --job-name=mesonh
+#SBATCH --time=0:05:00
+#SBATCH --partition=standard
+#SBATCH --qos=short
+#SBATCH --reservation=shortqos
+#SBATCH --account=d170
+#SBATCH --nodes=1
 
 . ../env.sh
 
 tmp_init $PWD/tmp
-
-cwltool --beta-dependency-resolvers-configuration $VESTEC_CWL_PLATFORM_CONF --enable-ext --mpi-config-file $VESTEC_CWL_MPI_CONF $VESTEC_CWL_ROOT/wildfire/mesonh.cwl mesonh.yml
-
-tmp_finalise
+cwltool \
+    --beta-dependency-resolvers-configuration $VESTEC_CWL_PLATFORM_CONF \
+    --enable-ext \
+    --preserve-environment LD_LIBRARY_PATH \
+    --mpi-config-file $VESTEC_CWL_MPI_CONF \
+    $VESTEC_CWL_ROOT/wildfire/mesonh.cwl \
+    mesonh.yml
