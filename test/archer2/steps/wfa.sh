caea6c7f37d855da2681e998144fbf890f75a47d
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-02-24 21:06:31 +0000

    get WFA running

--- test/archer2/steps/wfa.sh
+++ test/archer2/steps/wfa.sh
@@ -1,16 +1,28 @@
-#!/bin/bash --login
-#PBS -N WFA
-#PBS -l select=1
-#PBS -l walltime=0:20:00
-#PBS -q short
-#PBS -A d170
+#!/bin/bash
+#SBATCH --export=none
+#SBATCH --job-name=prep_pgd
+#SBATCH --time=0:20:00
+#SBATCH --partition=standard
+#SBATCH --qos=short
+#SBATCH --reservation=shortqos
+#SBATCH --account=d170
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --tasks-per-node=32
+#SBATCH --cpus-per-task=4
 
-cd $PBS_O_WORKDIR
+export OMP_NUM_THREADS=4
+export OMP_PLACES=cores
 
 . ../env.sh
 
-tmp_init $PWD/tmp
+cwltool  \
+    --beta-dependency-resolvers-configuration $VESTEC_CWL_PLATFORM_CONF \
+    --enable-ext \
+    --preserve-environment LD_LIBRARY_PATH \
+    --preserve-environment OMP_NUM_THREADS \
+    --preserve-environment OMP_PLACES \
+    --mpi-config-file $VESTEC_CWL_MPI_CONF \
+    $VESTEC_CWL_ROOT/wildfire/wfa.cwl \
+    wfa.yml
 
-cwltool  --beta-dependency-resolvers-configuration $VESTEC_CWL_PLATFORM_CONF --enable-ext --mpi-config-file $VESTEC_CWL_MPI_CONF $VESTEC_CWL_ROOT/wildfire/wfa.cwl wfa.yml
-
-tmp_finalise
