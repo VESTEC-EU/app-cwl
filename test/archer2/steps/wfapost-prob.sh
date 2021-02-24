1aaf8f241b05c6c7ddd43ad31439f97144c2c7d4
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-02-24 21:19:45 +0000

    WFA post proc tasks running

--- test/archer2/steps/wfapost-prob.sh
+++ test/archer2/steps/wfapost-prob.sh
@@ -1,22 +1,20 @@
-#!/bin/bash --login
-#PBS -N WFA-post
-#PBS -l select=1
-#PBS -l walltime=0:20:00
-#PBS -q short
-#PBS -A d170
+#!/bin/bash
+#
 # NOTE! This one doesn't have to run on compute nodes
-
-if [ -z ${PBS_O_WORKDIR+x} ]; then
-    # nothing
-    echo "nothing" > /dev/null
-else
-    cd $PBS_O_WORKDIR
-fi
+#
+#SBATCH --export=none
+#SBATCH --job-name=wfapost-prob
+#SBATCH --time=0:05:00
+#SBATCH --partition=standard
+#SBATCH --qos=short
+#SBATCH --reservation=shortqos
+#SBATCH --account=d170
+#SBATCH --nodes=1
 
 . ../env.sh
 
-tmp_init $PWD/tmp
+cwltool \
+    --beta-dependency-resolvers-configuration $VESTEC_CWL_PLATFORM_CONF \
+    $VESTEC_CWL_ROOT/wildfire/wfapost-prob.cwl \
+    wfapost-prob.yml
 
-cwltool --preserve-environment LD_LIBRARY_PATH --beta-dependency-resolvers-configuration $VESTEC_CWL_PLATFORM_CONF $VESTEC_CWL_ROOT/wildfire/wfapost-prob.cwl wfapost-prob.yml
-
-tmp_finalise
