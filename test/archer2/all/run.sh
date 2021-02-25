b3177c820a2308927d804efe132338ca5f9174db
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-02-25 10:36:09 +0000

    make all workflow run on A2

--- test/archer2/all/run.sh
+++ test/archer2/all/run.sh
@@ -1,23 +1,28 @@
-#!/bin/bash --login
-#PBS -N ALL
-#PBS -l select=1
-#PBS -l walltime=0:10:00
-#PBS -q short
-#PBS -A d170
-
-cd $PBS_O_WORKDIR
+#!/bin/bash
+#SBATCH --export=none
+#SBATCH --job-name=ALL
+#SBATCH --time=0:20:00
+#SBATCH --partition=standard
+#SBATCH --qos=short
+#SBATCH --reservation=shortqos
+#SBATCH --account=d170
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --tasks-per-node=32
+#SBATCH --cpus-per-task=4
 
 . ../env.sh
 
-tmp_init $PWD/tmp
+export OMP_NUM_THREADS=4
+export OMP_PLACES=cores
 
 cwltool \
     --preserve-environment LD_LIBRARY_PATH \
-    --relax-path-checks \
+    --preserve-environment OMP_NUM_THREADS \
+    --preserve-environment OMP_PLACES \
     --beta-dependency-resolvers-configuration $VESTEC_CWL_PLATFORM_CONF \
     --enable-ext \
     --mpi-config-file $VESTEC_CWL_MPI_CONF \
     $VESTEC_CWL_ROOT/wildfire/all.cwl \
     params.yml
 
-tmp_finalise
